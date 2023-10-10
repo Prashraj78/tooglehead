2d017b12297a1ec2730e6eaee00b0c4586a7eed3
Prashant Patil <prashantrajputaaaa@gmail.com>
2023-10-10 13:39:10 +0530

    almost done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@
 import './App.css';
 import { Accr } from './Components/Accr';
 import Footer from './Components/Footer';
+import Header from './Components/Header';
 import Navbar from './Components/Navbar';
 import Stats from './Components/Stats';
 import Subscription from './Components/Subscription';
 import Swipe from './Components/Swipe';
 import UpcomingExamination from './Components/UpcomingExamination';
- 
+
 
 function App() {
   return (
     <>
-    <Navbar/>
-
-   <UpcomingExamination/>
-   <Swipe/>
-    <Stats/>
-    <Accr />
-    <Subscription/>
-    <Footer/>
+      <Navbar />
+      <Header/>
+      <UpcomingExamination />
+      <Swipe />
+      <Stats />
+      <Accr />
+      <Subscription />
+      <Footer />
     </>
   );
 }
