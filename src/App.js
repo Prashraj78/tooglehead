af9905b91d825d16d16a3f4749b2ed7082d29761
Prashant Patil <prashantrajputaaaa@gmail.com>
2023-10-10 10:57:05 +0530

    added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Stats from './Components/Stats';
 import Subscription from './Components/Subscription';
+import UpcomingExamination from './Components/UpcomingExamination';
+ 
 
 function App() {
   return (
     <>
     <Navbar/>
     <Stats/>
+   <UpcomingExamination/>
     <Accr />
     <Subscription/>
     <Footer/>
---
+++ src/Components/UpcomingExamination.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+const UpcomingExamination = () => {
+
+  const ScheduleSvg = () => {
+    return (
+      <svg
+        width="50px"
+        height="50px"
+        viewBox="0 0 64 64"
+        strokeWidth="3"
+        stroke="#12406F"
+        fill="none"
+
+
+      >
+        <path d="M36.66,54.45H8.84A2.5,2.5,0,0,1,6.35,52V12.12A2.49,2.49,0,0,1,8.84,9.63H48.68a2.49,2.49,0,0,1,2.49,2.49v22.4" />
+        <line x1="6.35" y1="20.63" x2="51.17" y2="20.63" />
+        <line x1="16.46" y1="9.63" x2="16.46" y2="4.63" />
+        <line x1="40.42" y1="9.63" x2="40.42" y2="4.63" />
+        <circle cx="45.22" cy="45.44" r="12.43" />
+        <polyline points="45.22 36.7 45.22 45.82 49.57 49.16" />
+      </svg>
+    );
+  };
+  const schedule = [
+    { date: "02th October 2014", level: "Level 1 exam" },
+    { date: "Nov-Dec 2016", level: "Level 2 exam" },
+    { date: "02th October 2014", level: "Level 3 (Grad)" },
+  ];
+  return (
+    <div className="flex  justify-between flex-wrap sm:mx-16">
+      <div className="flex">
+      <div className="sm:flex flex-col text-start items-start ml-10 sm:ml-0">
+        <h1 className="upcoming-heading text-blue-800 text-2xl font-semibold">Upcoming<br />Examination</h1>
+        <p>Enquire about the examination &<br /> register for the exam</p>
+      </div>
+        <div className="hidden sm:block border-r border-blue-600 ml-8"> </div>
+
+        </div>
+      <div className="flex ">
+
+
+        <div className=" mx-4 sm:mx-0 flex text-start items-start mt-3">
+          {ScheduleSvg()}
+          <div className="ml-4 flex flex-col items-start">
+            <h1 className="schedule-heading font-semibold text-[#12406F] ">02th October 2014</h1>
+            <p>Level 1 exam</p>
+          </div>
+        </div>
+
+
+      </div>
+      <div className="flex ">
+
+
+        <div className=" mx-4 flex text-start items-start mt-2">
+          {ScheduleSvg()}
+          <div className="ml-4 flex flex-col items-start">
+            <h1 className="schedule-heading font-semibold text-[#12406F] ">Nov-Dec 2016 </h1>
+
+            <p>Level 2</p>
+            <p> Lorem Ipsum</p>
+            <p> Lorem Ipsum</p>
+          </div>
+        </div>
+
+
+      </div>
+      <div className="flex">
+
+
+        <div className=" mx-4 flex text-start items-start mt-2">
+          {ScheduleSvg()}
+          <div className="ml-4 flex flex-col items-start">
+            <h1 className="schedule-heading font-semibold text-[#12406F] ">Ongoing this year</h1>
+            <p> Level 3 (Grad)</p>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+
+
+  );
+};
+export default UpcomingExamination;
