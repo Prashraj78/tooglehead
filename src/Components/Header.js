45ad78ff7c2d1cbea572c1279e8314477c908aaa
Prashant Patil <prashantrajputaaaa@gmail.com>
2023-10-10 13:45:47 +0530

    resolving errors

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import headImg from '../Assests/head.png';
-import {BsPlay as FiPlay} from 'react-icons/bs';
+import { BsPlay as FiPlay } from 'react-icons/bs';
 
 const Header = () => {
     return (
         <div className='pt-[73px] bg-[#F6F6F6]'>
-            <div className='container ml-auto '>
+            <div className='container ml-auto'>
                 <div className='flex flex-col-reverse sm:flex-row items-center justify-between'>
-                    <div className='sm:w-[30%] w-[70%]' >
+                    <div className='sm:w-[30%] w-[100%] p-4'>
                         <p className='text-xl font-semibold my-3 text-[#12406F] text-center sm:text-left'>Lorem Ipsum</p>
                         <p className='text-center sm:text-left text-sm sm:text-base'>
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque fugiat earum dignissimos ex fuga velit aspernatur sapiente architecto neque, eius quos tempore dicta accusamus possimus error id iusto voluptate.
                         </p>
-                        <button className=' my-2 bg-[#12406F] text-white px-3 py-2 rounded-md flex'>Know more <FiPlay className='mt h-6 w-6'/> </button>
+                        <button className='my-2 bg-[#12406F] text-white px-3 py-2 rounded-md flex'>
+                            Know more <FiPlay className='mt-1 h-6 w-6' />
+                        </button>
                     </div>
                     <div className='flex justify-center sm:justify-end mt-4 sm:mt-0 sm:w-[35%]'>
                         <img src={headImg} alt="" className='w-32 h-32 sm:w-auto sm:h-auto hidden sm:block' />
@@ -20,9 +22,7 @@ const Header = () => {
                 </div>
             </div>
         </div>
-
-
-    )
+    );
 }
 
-export default Header
+export default Header;
